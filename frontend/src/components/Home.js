83dc783d56b941e52eb02f4a30642c39cfa17b40
Jessie Tang <tangqiaojie@hotmail.com>
2020-01-29 20:44:01 -0800

    edit

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,7 +1,5 @@
 import React from 'react';
-import { Tabs, Button, Spin } from 'antd';
-import { GEOLOCATION_OPTIONS, POSITION_KEY, API_ROOT, AUTH_HEADER, TOKEN_KEY } from '../constants';
-import { Gallery } from './Gallery';
+import { Tabs, Button } from 'antd';
 import '../styles/Home.css';
 
 const { TabPane } = Tabs;
@@ -9,9 +7,7 @@ const { TabPane } = Tabs;
 export class Home extends React.Component {
     state = {
         loadingGeolocation: false,
-        loadingPosts: false,
         errorMessage: null,
-        posts: [],
     }
 
     getGeolocation() {
@@ -21,9 +17,8 @@ export class Home extends React.Component {
         });
         if ('geolocation' in navigator) {
             navigator.geolocation.getCurrentPosition(
-                this.onGeolocationSuccess,  //call back function
+                this.onGeolocationSuccess,
                 this.onGeolocationFailure,
-                GEOLOCATION_OPTIONS,
             );
         } else {
             this.setState({
@@ -32,93 +27,18 @@ export class Home extends React.Component {
             });
         }
     }
+    onGeolocationSuccess = (position) => {
 
-    onGeolocationSuccess = (position) => {  //lamda function automatic bind (this.) get geolocation
-        this.setState({  //
-            loadingGeolocation: false,
-            errorMessage: null,
-        })
-        console.log(position);
-        const { latitude, longitude } = position.coords;
-        localStorage.setItem(POSITION_KEY, JSON.stringify({ latitude, longitude })); //convert string to json
-        this.loadNearbyPost();
     }
-
     onGeolocationFailure = () => {
-        this.setState({
-            loadingGeolocation: false,
-            errorMessage: 'Failed to load geolocation.',
-        })
+
     }
-
-    loadNearbyPost(
-        position = JSON.parse(localStorage.getItem(POSITION_KEY)),
-        range = 20,
-    ) {
-        this.setState({
-            loadingPosts: true,
-            errorMessage: null,
-        });
-        const token = localStorage.getItem(TOKEN_KEY);
-        fetch(`${API_ROOT}/search?lat=${position.latitude}&lon=${position.longitude}&range=${range}`, {
-            method: 'GET',
-            headers: {
-                Authorization: `${AUTH_HEADER} ${token}`,
-            },
-        }).then((response) => {
-            if (response.ok) {
-                return response.json();
-            }
-            throw new Error('Failed to load posts.');
-        }).then((data) => {
-            console.log(data);
-            this.setState({
-                loadingPosts: false,
-                posts: data ? data : [],
-            });
-        }).catch((error) => {
-            this.setState({
-                loadingPosts: false,
-                errorMessage: error.message,
-            });
-        });
-    }
-
-    getImagePosts() {
-        if (this.state.error) {
-            return <div>{this.state.error}</div>
-        } else if(this.state.loadingGeolocation) {
-            return <Spin tip="Loading geolocation..."/>
-        } else if (this.state.loadingPosts) {
-            return <Spin tip="Loading posts..." />
-        } else if (this.state.posts.length > 0) {
-            const images = this.state.posts.map((post) => {
-                return {
-                    user: post.user,
-                    src: post.url,
-                    thumbnail: post.url,
-                    caption: post.message,
-                    thumbnailWidth: 400,
-                    thumbnailHeight: 300,
-                }
-            });
-
-            return (<Gallery images={images}/>);
-        } else {
-            return 'No nearby posts.';
-        }
-    }
-
-    componentDidMount() {
-        this.getGeolocation();
-    }
-
     render() {
         const operations = <Button>Create New Post</Button>;
         return (
             <Tabs tabBarExtraContent={operations} className="main-tabs">
                 <TabPane tab="Image Posts" key="1">
-                    {this.getImagePosts()}
+                    content of tab 1
                 </TabPane>
                 <TabPane tab="Tab 2" key="2">
                     Content of tab 2
